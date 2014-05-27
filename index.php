570e7b2de0f5434f537a864f270a7c8b0235c768
Jon Mountjoy <jon@heroku.com>
2014-05-27 13:11:25 +0100

    twig

--- index.php
+++ index.php
@@ -12,6 +12,13 @@ $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 // add records to the log
 $log->addWarning("This message will be logged!");
 
+
 echo "Hello World!";
+
+
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
+
 
 ?>
