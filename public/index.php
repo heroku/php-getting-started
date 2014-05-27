8976b3adaf9799c18586bd9c525369a1e4673453
Jon Mountjoy <jon@heroku.com>
2014-05-27 13:45:37 +0100

    fix public

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require('vendor/autoload.php');
+require('../vendor/autoload.php');
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -16,12 +16,7 @@ $log->addWarning("This message will be logged!");
 echo "Hello World!";
 
 
-//$loader = new Twig_Loader_String();
-//$twig = new Twig_Environment($loader);
-//echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
-
-
-$loader = new Twig_Loader_Filesystem('templates');
+$loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader, array(
     'cache' => '/tmp/twig',
 ));
