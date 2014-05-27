815928c44399f4034167d6e8aeb2d959c40b5d20
Jon Mountjoy <jon@heroku.com>
2014-05-27 12:02:41 +0100

    add logging

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+require('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("This message will be logged!");
+
 echo "Hello World!";
 
 ?>
