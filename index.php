50aa9c464cd72c6da40eccc5ff96900d546ad771
Jon Mountjoy <jon@heroku.com>
2014-05-27 13:32:22 +0100

    twig

--- index.php
+++ index.php
@@ -16,9 +16,16 @@ $log->addWarning("This message will be logged!");
 echo "Hello World!";
 
 
-$loader = new Twig_Loader_String();
-$twig = new Twig_Environment($loader);
-echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
+//$loader = new Twig_Loader_String();
+//$twig = new Twig_Environment($loader);
+//echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
 
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => '/tmp/twig',
+));
+
+echo $twig->render('index.twig', array('name' => 'Jon was here'));
 
 ?>
