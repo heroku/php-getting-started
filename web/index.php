9ba28721ee94d5c4d59ed5e66f969c45a6b54365
Jon Mountjoy <jon@heroku.com>
2014-05-28 16:24:20 +0100

    clean up

--- web/index.php
+++ web/index.php
@@ -5,23 +5,26 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 //$app['debug'] = true;
 
+// Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register the Twig templating engine
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
 
-// ... definitions
+// Our web handlers
+
 $app->get('/', function () use ($app) {
-  $app['monolog']->addError('Testing the Monolog logging.');
+  $app['monolog']->addDebug('Monolog logging output.');
   return 'Hello';
 });
 
 $app->get('/foo/', function () use ($app) {
-  return 'HelloFoo';
+  return 'Some other output at /foo';
 });
 
 $app->get('/twig/{name}', function ($name) use ($app) {
@@ -30,26 +33,7 @@ $app->get('/twig/{name}', function ($name) use ($app) {
     ));
 });
 
-// ... definitions
-$app->get('/{name}', function ($name) use ($app) {
-  return 'Hello '.$app->escape($name);
-});
-
 
 $app->run();
 
-
 ?>
-<!--
-
-
-echo "Hello World!";
-
-
-$loader = new Twig_Loader_Filesystem('../templates');
-$twig = new Twig_Environment($loader, array(
-    'cache' => '/tmp/twig',
-));
-
-echo $twig->render('index.twig', array('name' => 'Jon was here'));
--->
