1d0bfda16278b9565b39db697041e8d60abb8f22
Jon Mountjoy <jon@heroku.com>
2014-05-30 14:23:19 +0100

    step-1

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # php-getting-started
 
-A barebones PHP bootstrap app that makes use of:
-
-* The [Silex](http://silex.sensiolabs.org/) web framework
-* [Twig](http://twig.sensiolabs.org/) templating
-* PDO database access
+A barebones PHP app that makes use of the [Silex](http://silex.sensiolabs.org/) web framework, which can easily be deployed to Heroku.
 
 ## Running Locally
 
 Make sure you have PHP, Apache and Composer installed.  Also, install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
 
 ```sh
-git clone git@github.com:jonmountjoy/php-getting-started.git # or clone your own fork
+git clone git@github.com:heroku/php-getting-started.git # or clone your own fork
 cd php-getting-started
 composer update
 foreman start web
@@ -23,17 +19,8 @@ Your app should now be running on [localhost:5000](http://localhost:5000/).
 
 ```
 heroku create
-heroku addons:add heroku-postgresql:dev
 git push heroku master
 heroku open
-```
-
-You may want to add some data to the database too:
-
-```
-heroku pg:psql
-> CREATE TABLE test_table (id integer, name text);
-> INSERT INTO test_table VALUES (1, 'hello world');
 ```
 
 ## Documentation
--- web/index.php
+++ web/index.php
@@ -10,50 +10,12 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register the Twig templating engine
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/../views',
-));
-
-// Register the Postgres database add-on
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-  array(
-    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-    'pdo.port' => $dbopts["port"],
-    'pdo.username' => $dbopts["user"],
-    'pdo.password' => $dbopts["pass"]
-  )
-);
-
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return 'Hello';
 });
-
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
-
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
-
-$app->get('/twig/{name}', function($name) use($app) {
-  return $app['twig']->render('index.twig', array(
-    'name' => $name,
-  ));
-});
-
 
 $app->run();
 
