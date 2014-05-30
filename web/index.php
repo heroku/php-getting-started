f273740147f4b0965dc43117fc77c50d92520ee2
Jon Mountjoy <jon@heroku.com>
2014-05-30 10:29:18 +0100

    cleanup whitespace

--- web/index.php
+++ web/index.php
@@ -12,29 +12,29 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 // Register the Twig templating engine
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+  'twig.path' => __DIR__.'/../views',
 ));
 
 // Register the Postgres database add-on
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
-    array(
-      'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-      'pdo.port' => $dbopts["port"],
-      'pdo.username' => $dbopts["user"],
-      'pdo.password' => $dbopts["pass"]
-    )
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
 );
 
 // Our web handlers
 
-$app->get('/', function () use ($app) {
+$app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return 'Hello';
 });
 
-$app->get('/db/', function () use ($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM blah');
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
   $st->execute();
 
   $names = array();
@@ -44,14 +44,14 @@ $app->get('/db/', function () use ($app) {
   }
 
   return $app['twig']->render('database.twig', array(
-        'names' => $names
+    'names' => $names
   ));
 });
 
-$app->get('/twig/{name}', function ($name) use ($app) {
-    return $app['twig']->render('index.twig', array(
-        'name' => $name,
-    ));
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
 });
 
 
