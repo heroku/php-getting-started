ed6a0064b4d0568f06bec07bab6961aa0a009722
Jon Mountjoy <tech@jonmountjoy.com>
2015-08-11 11:14:14 +0100

    new-ui

--- web/index.php
+++ web/index.php
@@ -10,11 +10,16 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  return $app['twig']->render('index.twig');
 });
 
 $app->run();
