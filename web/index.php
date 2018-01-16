99d688f398dc2e829afbccdc63a1bd5d59b88430
Jodi Henderson <jodi@crannmuir.co.uk>
2018-01-16 22:38:24 +0000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -17,9 +17,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+$app->get('/cowsay', function() use($app) { $app['monolog']->addDebug('cowsay'); return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>"; });
 
 $app->run();
