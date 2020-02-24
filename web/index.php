f8cb64cf458e6ab7f00126791732a7bb8e4ead70
zunzhong <54426972+zunzhong@users.noreply.github.com>
2020-02-24 11:34:58 +0700

    update zunzhong
    $app->get('/cowsay', function() use($app) {
      $app['monolog']->addDebug('cowsay');
      return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
    });

--- web/index.php
+++ web/index.php
@@ -23,3 +23,9 @@ $app->get('/', function() use($app) {
 });
 
 $app->run();
+
+//cowsay:
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
