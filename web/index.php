cc2ae6b11f921740a33ccbd37836e98ec20b0fc4
David Zuelke <dzuelke@salesforce.com>
2023-02-22 17:55:28 +0100

    use a DI container

--- web/index.php
+++ web/index.php
@@ -2,18 +2,25 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
 require(__DIR__.'/../vendor/autoload.php');
 
+// Create DI container
+$container = new Container();
+// Add Twig to Container
+$container->set('view', function() {
+  return Twig::create(__DIR__.'/views');
+});
+AppFactory::setContainer($container);
+
 // Create main Slim app
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, false, false);
-
-// Twig for Views
-$app->add(TwigMiddleware::create($app, Twig::create(__DIR__.'/views')));
+$app->add(TwigMiddleware::createFromContainer($app));
 
 // // Register the monolog logging service
 // $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -22,9 +29,8 @@ $app->add(TwigMiddleware::create($app, Twig::create(__DIR__.'/views')));
 
 // Our web handlers
 $app->get('/', function(Request $request, Response $response, $args) {
-  $twig = Twig::fromRequest($request);
   // $app['monolog']->addDebug('logging output.');
-  return $twig->render($response, 'index.twig');
+  return $this->get('view')->render($response, 'index.twig');
 });
 
 $app->run();
