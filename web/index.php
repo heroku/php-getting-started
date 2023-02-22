343ed3ca74eeb0e42a1a50205c8b2c5389a150f9
David Zuelke <dzuelke@salesforce.com>
2023-02-22 20:41:08 +0100

    Monolog via DI

--- web/index.php
+++ web/index.php
@@ -2,7 +2,11 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
 use DI\Container;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -15,6 +19,12 @@ $container = new Container();
 $container->set('view', function() {
   return Twig::create(__DIR__.'/views');
 });
+// Add Monolog to Container
+$container->set(LoggerInterface::class, function () {
+  $logger = new Logger('default');
+  $logger->pushHandler(new StreamHandler('php://stderr'), Level::Debug);
+  return $logger;
+});
 AppFactory::setContainer($container);
 
 // Create main Slim app
@@ -22,14 +32,9 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, false, false);
 $app->add(TwigMiddleware::createFromContainer($app));
 
-// // Register the monolog logging service
-// $app->register(new Silex\Provider\MonologServiceProvider(), array(
-//   'monolog.logfile' => 'php://stderr',
-// ));
-
 // Our web handlers
 $app->get('/', function(Request $request, Response $response, $args) {
-  // $app['monolog']->addDebug('logging output.');
+  $this->get(LoggerInterface::class)->debug('logging output.');
   return $this->get('view')->render($response, 'index.twig');
 });
 
