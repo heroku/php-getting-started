244b13136cfe0b139d24260c87aa935eda4309ea
David Zuelke <dzuelke@salesforce.com>
2023-02-23 19:39:47 +0100

    added database access

--- web/index.php
+++ web/index.php
@@ -30,6 +30,18 @@ $container->set(\Cowsayphp\AnimalInterface::class, function() {
   $class = '\\Cowsayphp\\Farm\\'.(getenv("COWSAY_FARM_CLASS")?:'Cow');
   return \Cowsayphp\Farm::create($class);
 });
+// Add Database connection to Container
+$container->set(PDO::class, function() {
+  $dburl = parse_url(getenv('DATABASE_URL') ?: throw new Exception('no DATABASE_URL'));
+  return new PDO(sprintf(
+    "pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s",
+    $dburl['host'],
+    $dburl['port'],
+    ltrim($dburl['path'], '/'), // URL path is the DB name, must remove leading slash
+    $dburl['user'],
+    $dburl['pass'],
+  ));
+});
 
 // Create main Slim app
 $app = Bridge::create($container);
@@ -44,6 +56,18 @@ $app->get('/coolbeans', function(Request $request, Response $response, LoggerInt
   $logger->debug('letting the Cowsay library write something cool.');
   $response->getBody()->write("<pre>".$animal->say("Cool beans")."</pre>");
   return $response;
+});
+$app->get('/db', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo) {
+  $st = $pdo->prepare('SELECT name FROM test_table');
+  $st->execute();
+  $names = array();
+  while($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $logger->debug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+  return $twig->render($response, 'database.twig', [
+    'names' => $names,
+  ]);
 });
 
 $app->run();
